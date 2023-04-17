f2946f557cc030bc8943379b327c4f91f71b426b
Александр Беспалов <bsplv@MacBook-Air-Aleksandr.local>
2023-04-17 21:30:31 +0300

    Complite the task4

---
+++ src/ru/vsuet/course1/task4/Main.java
@@ -0,0 +1,21 @@
+package ru.vsuet.course1.task4;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "Hello";
+        char ch = 'o';
+        int count = str_count(str, ch);
+        System.out.println(count); // выводим результат на экран
+    }
+
+    public static int str_count(String str, char ch) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ch) { // если текущий символ равен заданному
+                count++; // увеличиваем счетчик вхождений
+            }
+        }
+        return count;
+    }
+}
+
